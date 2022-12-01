e9e83cdba907371c09b978500e73345900a2328c
Lily-Xiong <xionglily0605@gmail.com>
2022-11-30 21:50:10 -0500

    -Removed random_walk and put random_move (from random_agent) into the class student agent instead.
    - modified check_endgame from world and use it in student_agent
    
    - added chessboard parameter in TreeNode

--- agents/student_agent.py
+++ agents/student_agent.py
@@ -80,66 +80,7 @@ class StudentAgent(Agent):
 
         return my_pos, dir
 
-    def check_endgame(self, board_size, chess_board, my_pos, adv_pos):
-        """
-        Check if the game ends and compute the current score of the agents.
 
-        Returns
-        -------
-        is_endgame : bool
-            Whether the game ends.
-        player_1_score : int
-            The score of player 1.
-        player_2_score : int
-            The score of player 2.
-        """
-        # Union-Find
-        father = dict()
-        for r in range(board_size):
-            for c in range(board_size):
-                father[(r, c)] = (r, c)
-
-        def find(pos):
-            if father[pos] != pos:
-                father[pos] = find(father[pos])
-            return father[pos]
-
-        def union(pos1, pos2):
-            father[pos1] = pos2
-
-        for r in range(board_size):
-            for c in range(board_size):
-                for dir, move in enumerate(
-                        self.moves[1:3]
-                ):  # Only check down and right
-                    if chess_board[r, c, dir + 1]:
-                        continue
-                    pos_a = find((r, c))
-                    pos_b = find((r + move[0], c + move[1]))
-                    if pos_a != pos_b:
-                        union(pos_a, pos_b)
-
-        for r in range(board_size):
-            for c in range(board_size):
-                find((r, c))
-        p0_r = find(tuple(my_pos))
-        p1_r = find(tuple(adv_pos))
-        p0_score = list(father.values()).count(p0_r)
-        p1_score = list(father.values()).count(p1_r)
-        if p0_r == p1_r:
-            return False, p0_score, p1_score
-        player_win = None
-        win_blocks = -1
-        if p0_score > p1_score:
-            player_win = 0
-            win_blocks = p0_score
-        elif p0_score < p1_score:
-            player_win = 1
-            win_blocks = p1_score
-        else:
-            player_win = -1  # Tie
-
-        return True, p0_score, p1_score
 
 class MonteCarloSearchTree():
     def __init__(self, move, parentNode):
@@ -161,12 +102,13 @@ class MonteCarloSearchTree():
 
 
 class TreeNode:
-    def __init__(self, move, parentNode=None):
+    def __init__(self, move, chessboard, parentNode=None):
         self.parent = parentNode
         self.children = []
         self.num_of_visit = 0
         self.num_of_wins = 0
         self.move = move
+        self.chessboard = chessboard
 
 
     # select the best child node using UCT
@@ -214,7 +156,7 @@ class TreeNode:
         new_node = TreeNode(parent_node, )
 
     def simulation(self, chess_board, max_step):
-
+        # returns the final board after the game has ended
 
         return end_state
 
@@ -280,6 +222,68 @@ class TreeNode:
                 state_queue.append((next_pos, cur_step + 1))
 
         return is_reached
+
+    def check_endgame(self, board_size, my_pos, adv_pos):
+        """
+        Check if the game ends and compute the current score of the agents.
+
+        Returns
+        -------
+        is_endgame : bool
+            Whether the game ends.
+        player_1_score : int
+            The score of player 1.
+        player_2_score : int
+            The score of player 2.
+        """
+        # Union-Find
+        moves = ((-1, 0), (0, 1), (1, 0), (0, -1))
+        father = dict()
+        for r in range(board_size):
+            for c in range(board_size):
+                father[(r, c)] = (r, c)
+
+        def find(pos):
+            if father[pos] != pos:
+                father[pos] = find(father[pos])
+            return father[pos]
+
+        def union(pos1, pos2):
+            father[pos1] = pos2
+
+        for r in range(board_size):
+            for c in range(board_size):
+                for dir, move in enumerate(
+                        moves[1:3]
+                ):  # Only check down and right
+                    if self.chessboard[r, c, dir + 1]:
+                        continue
+                    pos_a = find((r, c))
+                    pos_b = find((r + move[0], c + move[1]))
+                    if pos_a != pos_b:
+                        union(pos_a, pos_b)
+
+        for r in range(board_size):
+            for c in range(board_size):
+                find((r, c))
+        p0_r = find(tuple(my_pos))
+        p1_r = find(tuple(adv_pos))
+        p0_score = list(father.values()).count(p0_r)
+        p1_score = list(father.values()).count(p1_r)
+        if p0_r == p1_r:
+            return False, p0_score, p1_score
+        player_win = None
+        win_blocks = -1
+        if p0_score > p1_score:
+            player_win = 0
+            win_blocks = p0_score
+        elif p0_score < p1_score:
+            player_win = 1
+            win_blocks = p1_score
+        else:
+            player_win = -1  # Tie
+
+        return True, p0_score, p1_score
 
 
 class BoardState:
